e2f422b6d0e5c6267de10f1ec40db8b15c47e143
Evan <>
2020-05-05 12:22:14 +0800

    重構測試案例 在MineSweeperTest，提出mineSweeper到Property，並在每次測試前初始化。

--- app/src/test/java/evan/chen/tutorial/tdd/minesweeper/MineSweeperTest.kt
+++ app/src/test/java/evan/chen/tutorial/tdd/minesweeper/MineSweeperTest.kt
@@ -1,18 +1,25 @@
 package evan.chen.tutorial.tdd.minesweeper
 
 import org.junit.Assert
+import org.junit.Before
 import org.junit.Test
 
 class MineSweeperTest {
+
+    lateinit var mineSweeper : MineSweeper
+
+    @Before
+    fun setup(){
+        mineSweeper = MineSweeper()
+    }
 
     @Test
     fun startGame() {
         val level = 9
         val cellCreator = CellCreator()
         cellCreator.level = level
-        val mineSweeper = MineSweeper()
         mineSweeper.startGame(cellCreator)
-        val cells:List<Cell> = mineSweeper.cells
+        val cells: List<Cell> = mineSweeper.cells
         Assert.assertEquals(81, cells.count())
     }
 }
