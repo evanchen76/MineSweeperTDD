67bcf8846492047f12d4915438490e4d51c84113
Evan <>
2020-05-06 11:15:02 +0800

    重構：用createCell產生測試用的方格

--- app/src/test/java/evan/chen/tutorial/tdd/minesweeper/MineSweeperTest.kt
+++ app/src/test/java/evan/chen/tutorial/tdd/minesweeper/MineSweeperTest.kt
@@ -6,10 +6,10 @@ import org.junit.Test
 
 class MineSweeperTest {
 
-    lateinit var mineSweeper : MineSweeper
+    lateinit var mineSweeper: MineSweeper
 
     @Before
-    fun setup(){
+    fun setup() {
         mineSweeper = MineSweeper()
     }
 
@@ -27,9 +27,9 @@ class MineSweeperTest {
     fun tapCellShouldOpen() {
         //點擊會被打開
         //產生1*1個Cell
-        val cells = mutableListOf<Cell>(
-            Cell().apply { x=0; y=0 }
-        )
+        val init = mutableListOf<String>()
+        init.add("-")
+        val cells = createCell(init)
 
         val creator = FakeCellCreator()
         creator.cells = cells
@@ -40,5 +40,25 @@ class MineSweeperTest {
         mineSweeper.tap(x, y)
         val cell = mineSweeper.cells.find { it.x == x && it.y == y }
         Assert.assertEquals(Cell.Status.OPEN, cell?.status)
+    }
+
+    private fun createCell(initSweeper: MutableList<String>): MutableList<Cell> {
+        //將圖示字串轉為MutableList<Cell>
+        val cells: MutableList<Cell> = mutableListOf()
+        initSweeper.forEachIndexed { yIndex, yList ->
+            val lines = yList.split("|")
+            lines.forEachIndexed { xIndex, value ->
+                val cell = Cell()
+                cell.x = xIndex
+                cell.y = yIndex
+                cell.status = Cell.Status.CLOSE
+                if (value == " ") {
+                    cell.status = Cell.Status.OPEN
+                }
+                cells.add(cell)
+            }
+        }
+
+        return cells
     }
 }
